7053af68e0cffd2b2d1977148453e68f13626d08
root <root@ip-10-0-1-10.ap-northeast-1.compute.internal>
2022-03-18 02:46:27 +0000

    写真フォルダを移動

--- server/manage_weight/resources/views/auth/login.blade.php
+++ server/manage_weight/resources/views/auth/login.blade.php
@@ -1,71 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card text-info border-info">
-                <div class="card-header bg-white border-info">ログイン画面</div>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card text-info border-info">
+            <div class="card-header bg-white border-info">ログイン画面</div>
 
-                <div class="card-body">
-                    @if($errors->any())
-                    <div class="alert alert-danger">
-                        @foreach($errors->all() as $message)
-                        <p>{{$message}}</p>
-                        @endforeach
-                    </div>
-                    @endif
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control border-info" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control border-info" name="password" required autocomplete="current-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary w-100">
-                                    ログイン
-                                </button>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 offset-md-4">
-                                    <a href="{{ route('login.guest') }}" class="btn btn-primary w-100">
-                                        ゲストログイン
-                                    </a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 offset-md-4 mt-3">
-                                <a class="border-bottom" href="{{ route('register') }}">
-                                    会員登録はこちらから
-                                </a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 offset-md-4 mt-2">
-                                    @if (Route::has('password.request'))
-                                    <a class="border-bottom" href="{{ route('password.request') }}">
-                                        パスワードを忘れた
-                                    </a>
-                                    @endif
-                            </div>
-                        </div>
-                    </form>
+            <div class="card-body">
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    @foreach($errors->all() as $message)
+                    <p>{{$message}}</p>
+                    @endforeach
                 </div>
+                @endif
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="form-group row">
+                        <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control border-info" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control border-info" name="password" required autocomplete="current-password">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary w-100">
+                                ログイン
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-4">
+                                <a href="{{ route('login.guest') }}" class="btn btn-primary w-100">
+                                    ゲストログイン
+                                </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-4 mt-3">
+                            <a class="border-bottom" href="{{ route('register') }}">
+                                会員登録はこちらから
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 offset-md-4 mt-2">
+                                @if (Route::has('password.request'))
+                                <a class="border-bottom" href="{{ route('password.request') }}">
+                                    パスワードを忘れた
+                                </a>
+                                @endif
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- server/manage_weight/resources/views/auth/register.blade.php
+++ server/manage_weight/resources/views/auth/register.blade.php
@@ -1,64 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card text-info border-info">
-                <div class="card-header bg-white border-info">会員登録</div>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card text-info border-info">
+            <div class="card-header bg-white border-info">会員登録</div>
 
-                <div class="card-body">
-                    @if($errors->any())
-                    <div class="alert alert-danger">
-                        @foreach($errors->all() as $message)
-                        <p>{{ $message }}</p>
-                        @endforeach
-                    </div>
-                    @endif
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">ユーザー名</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control border-info" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control border-info" name="email" value="{{ old('email') }}" required autocomplete="email">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control border-info" name="password" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード(確認)</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control border-info" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    登録
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="card-body">
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    @foreach($errors->all() as $message)
+                    <p>{{ $message }}</p>
+                    @endforeach
                 </div>
+                @endif
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <div class="form-group row">
+                        <label for="name" class="col-md-4 col-form-label text-md-right">ユーザー名</label>
+
+                        <div class="col-md-6">
+                            <input id="name" type="text" class="form-control border-info" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control border-info" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control border-info" name="password" required autocomplete="new-password">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード(確認)</label>
+
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control border-info" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                登録
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
